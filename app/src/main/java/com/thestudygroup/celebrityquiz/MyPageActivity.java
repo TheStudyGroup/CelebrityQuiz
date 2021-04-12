3a631c48c410382e97d11ee2d81fc002c22f68bd
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-04-12 14:33:58 +0900

    Add some activities

---
+++ app/src/main/java/com/thestudygroup/celebrityquiz/MyPageActivity.java
@@ -0,0 +1,15 @@
+package com.thestudygroup.celebrityquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPageActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
