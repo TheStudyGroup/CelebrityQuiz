2b95820f9e20617f4eb90c7283900b05faeacfbb
godgichan <74453259+godgichan@users.noreply.github.com>
2021-04-16 15:37:43 +0900

    AddGlobalLogin

---
+++ app/src/main/java/com/thestudygroup/celebrityquiz/activity/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.thestudygroup.celebrityquiz.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.thestudygroup.celebrityquiz.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+    private Button sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        sign_up = findViewById(R.id.register_btn_login);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    // When initializing your Activity, check to see if the user is currently signed in.
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
+    private void signUp() {
+        // 이메일
+        EditText emailEditText = findViewById(R.id.register_id);
+        String email = emailEditText.getText().toString();
+        // 비밀번호
+        EditText passwordEditText = findViewById(R.id.register_pw);
+        String password = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(RegisterActivity.this, "등록 완료", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+    }
+
+    Intent intent = getIntent();
+
+}
